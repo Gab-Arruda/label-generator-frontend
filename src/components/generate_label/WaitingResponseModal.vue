2d76edce39d3bd452137ae0bbbe1ccaeabc9b98c
gabriel.santos <gabriel.santos@vegamonitoramento.com.br>
2023-10-29 17:18:12 -0300

    Added modal when loading page

---
+++ src/components/generate_label/WaitingResponseModal.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'Realizando operações...'
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="modal-container">
+        <div class="modal">
+            <div class="flex justify-center">
+                <p class="text-3xl text-slate-600">{{ this.title }}</p>
+            </div>
+            <img type="image" src="../../assets/loading-screen.gif" alt="carregando" />
+        </div>
+    </div>
+</template>
+
+<style>
+  .modal-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9998;
+  }
+
+  .modal {
+    position: relative; 
+    background: white;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  }
+</style>
--- src/pages/GenerateLabelPage.vue
+++ src/pages/GenerateLabelPage.vue
@@ -3,37 +3,39 @@
     import ChooseIngredients from '../components/generate_label/ChooseIngredients.vue';
     import IngredientsList from '../components/generate_label/IngredientsList.vue';
     import ReferenceValue from '../components/generate_label/ReferenceValue.vue';
+    import WaitingResponseModal from '../components/generate_label/WaitingResponseModal.vue';
     import axios from 'axios';
     export default {
         data() {
             return {
-                waitingAPI: false
+                waitingAPI: false,
+                modalTitle: 'Buscando lista de alimentos...'
             }
         },
         components: {
             RecipeTitle,
             ChooseIngredients,
             IngredientsList,
-            ReferenceValue
+            ReferenceValue,
+            WaitingResponseModal
         },
         mounted() {
+            this.waitingAPI = true;
             this.getFoodList();
             this.getCategoriesList();
         },
         methods: {
             async getFoodList() {
+                this.waitingAPI = true;
                 try {
-                    this.waitingAPI = true;
                     const { data } = await axios.get('http://localhost:8000/api/food/');
                     this.$store.commit('setFoodList', data);
                 } catch (error) {
                     alert('Error fetching food list: ' + error.message);
                 }
-                this.waitingAPI = false;
             },
             async getCategoriesList() {
                 try {
-                    this.waitingAPI = true;
                     const { data } = await axios.get('http://localhost:8000/api/category/');
                     this.$store.commit('setCategoriesList', data);
                 } catch (error) {
@@ -53,5 +55,6 @@
             <IngredientsList/>
             <ReferenceValue/>
         </div>
+        <WaitingResponseModal v-if="waitingAPI" :title="modalTitle"/>
     </div>
 </template>
