b17cd257b5264ea5efc18cc5f6cc892a498ce893
gabriel.santos <gabriel.santos@vegamonitoramento.com.br>
2023-12-26 17:41:08 -0300

    Styles border of table

--- src/components/generate_label/Label.vue
+++ src/components/generate_label/Label.vue
@@ -8,17 +8,12 @@ export default {
         }
     },
     mounted() {
-
+        window.print();
     },
     computed: {
         ...mapState(['combined_nutrients']),
         ...mapState(['reference']),
         ...mapState(['recipe_mass_when_done'])
-    },
-    methods: {
-        printLabel() {
-            window.print();
-        }
     }
 }
 </script>
@@ -28,30 +23,26 @@ export default {
         <!-- ALERTAS -->
 
         <!-- TABELA NUTRICIONAL -->
-        <div class="flex flex-col w-1/2">
-            <h1 class="font-bold self-center">INFORMAÇÂO NUTRICIONAL</h1>
-            <div class="flex flex-col items-center">
-                <span>Porções por embalagem: {{ Math.floor(recipe_mass_when_done / reference.value) }}</span>
-                <span>Porção de {{reference.value}}g ({{ reference.quantity }} {{ reference.homemade_measure }})</span>
+        <div class="flex flex-col w-1/2 my-8">
+            <h1 class="font-bold self-center border border-b-0 border-slate-800 w-full text-center">INFORMAÇÂO NUTRICIONAL</h1>
+            <div class="flex flex-col items-center border border-b-0 border-slate-800 ">
+                <span class="w-full text-center">Porções por embalagem: {{ Math.floor(recipe_mass_when_done / reference.value) }}</span>
+                <span class="w-full text-center">Porção de {{reference.value}}g ({{ reference.quantity }} {{ reference.homemade_measure }})</span>
             </div>
             <table>
-                <tr>
+                <tr class="border border-slate-800">
                     <th></th>
                     <th>100g</th>
                     <th>Porção</th>
                     <th>%VD</th>
                 </tr>
-                <tr v-for="key in Object.keys(combined_nutrients)" :key="key">
-                    <td>{{ key }}</td>
-                    <td class="text-center">{{ combined_nutrients[key].hundredGram }}</td>
-                    <td class="text-center">{{ combined_nutrients[key].referenceValue }}</td>
-                    <td class="text-center">{{ combined_nutrients[key].vdrValue }}</td>
+                <tr v-for="key in Object.keys(combined_nutrients)" :key="key" class="border border-slate-800">
+                    <td class="px-1">{{ key }}</td>
+                    <td class="text-center px-1">{{ combined_nutrients[key].hundredGram }}</td>
+                    <td class="text-center px-1">{{ combined_nutrients[key].referenceValue }}</td>
+                    <td class="text-center px-1">{{ combined_nutrients[key].vdrValue }}</td>
                 </tr>
             </table>
         </div>
-
-        <button class="bg-blue-400 text-white self-center text-xl font-bold px-4 py-2 my-8 rounded-md hover:cursor-pointer hover:bg-blue-200"
-            @click="printLabel()">Imprimir
-        </button>
     </div>
 </template>
